fd0ac20debbb3354ab1c7f0b62dd4f71ebad9f71
Feng Zhou <zf.pascal@gmail.com>
2015-12-24 13:56:34 +0800

    venv-switch

--- emacs/.el/theme-conf.el
+++ emacs/.el/theme-conf.el
@@ -18,6 +18,11 @@
 ;; (when (member "DejaVu Sans Mono" (font-family-list))
 ;;   (set-face-attribute 'default nil :font "DejaVu Sans Mono-10"))
 
+;; (set-face-attribute 'default nil :font "")
+
+(set-default-font "DejaVu Sans Mono-10")
+(set-fontset-font nil 'chinese-gbk '"WenQuanYi Micro Hei Mono-12")
+
 ;; (when (member "Fira Code" (font-family-list))
 ;;   (set-face-attribute 'default nil :font "Fira Code-10" :weight 'Light))
 
@@ -25,7 +30,7 @@
 
 ;; (set-face-attribute 'default nil :font "Input Mono Light 11" :weight 'Thin)
 
-(set-face-attribute 'default nil :font "Source Code Pro 11")
+;; (set-face-attribute 'default nil :font "Source Code Pro 10")
 
 (use-package powerline :ensure t)
 (setq powerline-default-separator 'wave)
